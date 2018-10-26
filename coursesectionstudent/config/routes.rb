d1a0afb293d02485f1a170d099f80855ee47b291
Joe <qtangle@gmail.com>
2018-10-25 19:21:39 -0600

    Navbar functionality added

--- coursesectionstudent/config/routes.rb
+++ coursesectionstudent/config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
+  root to: 'enrollments#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
